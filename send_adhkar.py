fde22c613aa47ef44cc7f1aa91886e3fdcf32de2
Abdo-Th <159192502+Abdo-Th@users.noreply.github.com>
2025-05-23 16:53:07 +0200

    Update send_adhkar.py

--- send_adhkar.py
+++ send_adhkar.py
@@ -1,19 +1,18 @@
-import os
 import requests
+import os
 
-def send_dhikr():
-    token = os.getenv("TELEGRAM_BOT_TOKEN")
-    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
-    if not token or not chat_id:
-        print("Missing Telegram credentials.")
-        return
-
-    text = "لَا إلَهَ إلَّا اللَّهُ وَحْدَهُ لَا شَرِيكَ لَهُ، لَهُ المُلكُ ولَهُ الحمدُ يُحيِي ويُميتُ وهو علَى كلِّ شيءٍ قدير"
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-    data = {"chat_id": chat_id, "text": text}
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
     response = requests.post(url, data=data)
-    print(response.json())
+    print(response.text)
 
-if __name__ == "__main__":
-    send_dhikr()
+# الدعاء المراد إرساله
+adhkar = "لَا إِلَهَ إِلَّا اللَّهُ وَحْدَهُ لَا شَرِيكَ لَهُ، لَهُ المُلكُ ولَهُ الحمدُ يُحيِي ويُميتُ وهو علَى كلِّ شيءٍ قدير"
+send_telegram_message(adhkar)
